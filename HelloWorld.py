fffa2900dd60b3d21d45be72e73f1891011468f3
sabralod <noreply@simongeyer.com>
2017-06-22 10:18:52 +0200

    Reset to start.

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,7 +1,2 @@
-#name = "Simon"
-
-file = open("name.txt", "r")
-for line in file:
-    print("Hello, %s!" % line.rstrip('\n'))
-
-#print("Hello, %s!" % name)
+name = "Simon"
+print("Hello, %s!" % name)
